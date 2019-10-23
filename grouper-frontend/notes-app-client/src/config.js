c36519a7a6e7621750cb6048ddff40de837fd94d
Gabe Campbell <gabe.campbell@icloud.com>
2019-10-23 18:52:51 -0500

    Setting up the React app

---
+++ grouper-frontend/notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "grouper-notes-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://kzrynkxrzl.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_k0irhCryd",
+    APP_CLIENT_ID: "77ngu91s9kd50of8v66ph8avup",
+    IDENTITY_POOL_ID: "us-east-2:f90f3900-fa58-47f6-93bb-8b21e3071bc9"
+  }
+};
