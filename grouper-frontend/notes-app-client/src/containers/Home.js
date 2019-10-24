1e19cd7ee72d58510a38f42916003e0e03ff8b04
Gabe Campbell <gabe.campbell@icloud.com>
2019-10-24 15:14:15 -0500

    Committing changes before demo

--- grouper-frontend/notes-app-client/src/containers/Home.js
+++ grouper-frontend/notes-app-client/src/containers/Home.js
@@ -54,8 +54,8 @@ export default function Home(props) {
   function renderLander() {
     return (
       <div className="lander">
-        <h1>Scratch</h1>
-        <p>A simple note taking app</p>
+        <h1>Grouper</h1>
+        <p>Note taking application</p>
       </div>
     );
   }
